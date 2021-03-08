abfd945e824bac0da9b93a398db63244a08a5368
Mario Hartleb <mhartleb83@yahoo.com>
2021-03-08 18:50:26 +0100

    int3

---
+++ intermediate/int3.js
@@ -0,0 +1,20 @@
+let names = ["Martin", "Thomas","Klaus","Maria","David"];
+let point = [76,85,65,93,81];
+
+
+
+
+// for (let i = 0; i < point.length; i++) {
+//     if (points[i]<101 && points[i]>=90) {
+//         return `${names[i]} has ${points[i]}. It's an A.`
+//                     }
+
+
+//     console.log(array[i]);
+
+// }
+
+
+
+
+
